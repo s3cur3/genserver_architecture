29ef5394ccd06ea2876fe034aa666fdbdb13240e
Tyler Young <tyoung@enbala.com>
2021-10-05 20:40:26 -0500

    v1.5: Application env to give a server name for each test

---
+++ v1.5/lib/virtual_power_plant.ex
@@ -0,0 +1,90 @@
+defmodule VirtualPowerPlant do
+  @moduledoc """
+  A "virtual power plant" is a collection of grid-connected batteries
+  that we can control remotely.
+
+  We ask these batteries to either pump power onto to the grid to meet
+  increased demand, or we ask them to absorb and store power that was
+  generated in excess of demand (e.g., by solar installations).
+  """
+  use GenServer
+
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, [], name: server_name())
+  end
+
+  defp server_name do
+    Application.get_env(:energy_application, :vpp_name, __MODULE__)
+  end
+
+  @doc """
+  Associates this battery with our virtual power plant, allowing us to
+  control it to meet grid needs.
+  """
+  def add_battery(battery) do
+    GenServer.call(server_name(), {:add_battery, battery})
+  end
+
+  @doc "Collection of IDs for the batteries we control"
+  def batteries do
+    GenServer.call(server_name(), :batteries)
+  end
+
+  @doc """
+  The total wattage our virtual power plant is contributing to the grid
+  (positive values) or absorbing off the grid (negative values).
+  """
+  def current_power do
+    GenServer.call(server_name(), :total_power)
+  end
+
+  @doc """
+  Attempt to change our batteries' state to contribute this many watts to the grid.
+  Overrides any previous requests.
+  """
+  def export(watts) do
+    GenServer.call(server_name(), {:set_power, watts})
+  end
+
+  @doc """
+  Attempt to change our batteries' state to absorb this many watts to the grid.
+  Overrides any previous requests.
+  """
+  def absorb(watts) do
+    GenServer.call(server_name(), {:set_power, -watts})
+  end
+
+  # Server implementation
+  def init(_) do
+    {:ok, []}
+  end
+
+  def handle_call({:add_battery, battery}, _from, battery_collection) do
+    updated_state = [battery | battery_collection]
+    {:reply, :ok, updated_state}
+  end
+
+  def handle_call(:batteries, _from, battery_collection) do
+    {:reply, battery_collection, battery_collection}
+  end
+
+  def handle_call(:total_power, _from, battery_collection) do
+    total_power =
+      battery_collection
+      |> Enum.map(&Battery.current_power/1)
+      |> Enum.sum()
+
+    {:reply, total_power, battery_collection}
+  end
+
+  def handle_call({:set_power, needed_watts}, _from, battery_collection) do
+    _unmet_need =
+      Enum.reduce(battery_collection, needed_watts, fn battery, remaining_need ->
+        actual_setpoint = Battery.update_current_power(battery, remaining_need)
+        needed_watts - actual_setpoint
+      end)
+
+    {:reply, :ok, battery_collection}
+  end
+
+end
---
+++ v1.5/test/virtual_power_plant_test.exs
@@ -0,0 +1,52 @@
+defmodule VirtualPowerPlantTest do
+  use ExUnit.Case
+
+  setup do
+    Application.put_env(:energy_application, :vpp_name, random_atom())
+    VirtualPowerPlant.start_link([])
+    :ok
+  end
+
+  defp random_atom do
+    random_string = for _ <- 1..10, into: "", do: <<Enum.random('0123456789abcdef')>>
+    String.to_atom(random_string)
+  end
+
+  test "aggregates batteries" do
+    {:ok, _pid1} = Battery.create("battery_1", 100)
+    {:ok, _pid2} = Battery.create("battery_2", 100)
+
+    VirtualPowerPlant.add_battery("battery_1")
+    VirtualPowerPlant.add_battery("battery_2")
+
+    batteries = VirtualPowerPlant.batteries()
+    assert Enum.sort(batteries) == ["battery_1", "battery_2"]
+  end
+
+  test "current_power returns the sum of all asset's current_power" do
+    {:ok, _pid1} = Battery.create("battery_1", 100, 8)
+    {:ok, _pid2} = Battery.create("battery_2", 100, 3)
+
+    VirtualPowerPlant.add_battery("battery_1")
+    VirtualPowerPlant.add_battery("battery_2")
+
+    assert VirtualPowerPlant.current_power() == 11
+  end
+
+#  test "adds batteries" do
+#    {:ok, _pid1} = Battery.create("battery_1", 100)
+#    {:ok, _pid2} = Battery.create("battery_2", 100)
+#
+#    Application.put_env(:energy_application, :vpp, :ok)
+#
+#    :global.register_name(:vpp, self())
+#    on_exit(fn -> :global.unregister_name(:vpp) end)
+#
+#    {:ok, task_pid} =
+#      Task.start(fn ->
+#        VirtualPowerPlant.add_battery({:global, :vpp}, "battery_1")
+#      end)
+#
+#    assert_receive {:"$gen_call", {^task_pid, _}, {:add_battery, "battery_1"}}, 500
+#  end
+end

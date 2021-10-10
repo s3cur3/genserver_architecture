7b49a18a01e1eb2e49aecae39674e8ea5c7b0325
Tyler Young <tyoung@enbala.com>
2021-10-10 06:20:11 -0500

    v2 retains broken sum_of_power/1 (fixed in v3)

--- v2/test/virtual_power_plant_test.exs
+++ v2/test/virtual_power_plant_test.exs
@@ -5,7 +5,7 @@ defmodule VirtualPowerPlantTest do
     {:ok, _pid1} = Battery.create("battery_1", 100)
     {:ok, _pid2} = Battery.create("battery_2", 100)
 
-    {:ok, vpp} = VirtualPowerPlant.start_link(name: :test1)
+    {:ok, vpp} = VirtualPowerPlant.start_link(name: :aggregate_batteries)
     VirtualPowerPlant.add_battery(vpp, "battery_1")
     VirtualPowerPlant.add_battery(vpp, "battery_2")
 
@@ -17,7 +17,7 @@ defmodule VirtualPowerPlantTest do
     {:ok, _pid1} = Battery.create("battery_1", 100, 8)
     {:ok, _pid2} = Battery.create("battery_2", 100, 3)
 
-    {:ok, vpp} = VirtualPowerPlant.start_link(name: :test1)
+    {:ok, vpp} = VirtualPowerPlant.start_link(name: :current_power)
     VirtualPowerPlant.add_battery(vpp, "battery_1")
     VirtualPowerPlant.add_battery(vpp, "battery_2")
 
@@ -27,7 +27,7 @@ defmodule VirtualPowerPlantTest do
   test "exports power to the grid to meet a demand" do
     {:ok, _pid1} = Battery.create("battery_1", 100)
 
-    {:ok, vpp} = VirtualPowerPlant.start_link(name: :test1)
+    {:ok, vpp} = VirtualPowerPlant.start_link(name: :export)
     VirtualPowerPlant.add_battery(vpp, "battery_1")
 
     VirtualPowerPlant.export(vpp, 5)
@@ -38,7 +38,7 @@ defmodule VirtualPowerPlantTest do
   test "absorbs power to the grid to meet a demand" do
     {:ok, _pid1} = Battery.create("battery_1", 100)
 
-    {:ok, vpp} = VirtualPowerPlant.start_link(name: :test1)
+    {:ok, vpp} = VirtualPowerPlant.start_link(name: :absorb)
     VirtualPowerPlant.add_battery(vpp, "battery_1")
 
     VirtualPowerPlant.absorb(vpp, 5)
@@ -50,14 +50,38 @@ defmodule VirtualPowerPlantTest do
     {:ok, _pid1} = Battery.create("battery_1", 100)
     {:ok, _pid2} = Battery.create("battery_2", 50)
 
-    {:ok, vpp} = VirtualPowerPlant.start_link(name: :test1)
+    {:ok, vpp} = VirtualPowerPlant.start_link(name: :respect_max_power)
     VirtualPowerPlant.add_battery(vpp, "battery_1")
     VirtualPowerPlant.add_battery(vpp, "battery_2")
 
     VirtualPowerPlant.absorb(vpp, 1_000)
-    assert VirtualPowerPlant.current_power(vpp) == -150
+    assert sum_of_power(["battery_1", "battery_2"]) == -150
 
     VirtualPowerPlant.export(vpp, 1_000)
-    assert VirtualPowerPlant.current_power(vpp) == 150
+    assert sum_of_power(["battery_1", "battery_2"]) == 150
   end
+
+  defp sum_of_power(battery_ids) do
+    battery_ids
+    |> Enum.map(&Battery.current_power/1)
+    |> Enum.sum()
+  end
+
+  #defp sum_of_power(battery_ids) do
+  #  sync_batteries = fn ->
+  #    battery_ids
+  #    |> Enum.map(fn id ->
+  #      BatteryRegistry.whereis_name({Battery, id})
+  #    end)
+  #    |> Enum.each(&:sys.get_state/1)
+  #  end
+  #
+  #  sync_batteries.()
+  #  :sys.get_state(VirtualPowerPlant)
+  #  sync_batteries.()
+  #
+  #  battery_ids
+  #  |> Enum.map(&Battery.current_power/1)
+  #  |> Enum.sum()
+  #end
 end
